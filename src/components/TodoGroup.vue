7d7e96595bb2cdc2333bc4af44d1a4dd5bee2f3c
Dwayne Neckles <dwayneaneckles@gmail.com>
2019-09-10 13:35:20 -0400

    created simple todo list

---
+++ src/components/TodoGroup.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<div v-bind:key="todoItem.id" v-for="todoItem in todoGroupProps">
+<TodoItem v-bind:todoItem="todoItem"  v-on:del-todo="$emit('del-todo',todoItem.id)"/>
+</div>
+</div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: "TodoGroup",
+    components: {
+        TodoItem
+    },
+    props: ["todoGroupProps"]
+}
+</script>
+ 
+<style scoped>
+
+</style>
